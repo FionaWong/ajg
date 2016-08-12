bce65e6d3e042046d25a736fe5dfc7e16bcd39a7
dongson <825826837@qq.com>
2016-08-12 10:12:25 +0800

    添加newproduct文件

---
+++ src_jq/js/views/goods/newproduct.js
@@ -0,0 +1,13 @@
+(function(window,$){
+  $(document).ready(function(){
+    var moduleId = '1';
+    //左菜单设置
+    common.setActive(moduleId,true);
+    common.appendTo($('.sidebar'));
+
+
+
+
+  });
+
+})(window,$);
